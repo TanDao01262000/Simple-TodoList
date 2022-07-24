47a4b88386e83dacde9567795a7592503eecbee3
tankhanhdao <tankhanhdao@gmail.com>
2022-07-24 08:41:14 -0700

    first commit 7/24/22

---
+++ mysite/app1/views.py
@@ -0,0 +1,10 @@
+from msilib import init_database
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import TodoList, Item
+
+# Create your views here.
+
+def index(response, id):
+    ls = TodoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" % ls.name) 
